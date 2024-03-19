f04fc051b2525cee37a93dca238cf8b2a08c9d9c
Deborah Kurz <DeborahKurz6@gmail.com>
2024-03-19 10:50:48 -0600

    Test 2 Complete

--- lib/functions.py
+++ lib/functions.py
@@ -4,10 +4,10 @@ def greet_programmer():
     print('Hello, programmer!')
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
     pass
